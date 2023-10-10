b1de0c19167c6cc9de4189b2e1b6ad058014d8d6
Ali Orynbasar <96694797+ssendamevil@users.noreply.github.com>
2023-10-10 23:56:10 +0600

    Solved conditions1-2

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -1,8 +1,39 @@
 package com.epam.conditions;
 
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        int winter[] = {12,1,2};
+        int spring[] = {3,4,5};
+        int summer[] = {6,7,8};
+
+        IntPredicate check = (m) -> {
+            if(m == monthNumber)
+                return true;
+            return false;
+        };
+
+        if(monthNumber > 0 && monthNumber < 13){
+            if(Arrays.stream(winter).anyMatch(check)){
+                System.out.println("Winter");
+            }else if(Arrays.stream(spring).anyMatch(check)){
+                System.out.println("Spring");
+            }else if(Arrays.stream(summer).anyMatch(check)){
+                System.out.println("Summer");
+            }else {
+                System.out.println("Autumn");
+            }
+        }else{
+            System.out.println("Wrong month number");
+        }
+    }
+
+    public static void main(String[] args) {
+        SeasonDeterminer seasonDeterminer = new SeasonDeterminer();
+        seasonDeterminer.tellTheSeason(0);
     }
 
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,20 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if((firstSide + secondSide) < thirdSide){
+            System.out.println("it's not a triangle");
+        }else if((secondSide + thirdSide) < firstSide){
+            System.out.println("it's not a triangle");
+        }else if((firstSide+thirdSide) < secondSide){
+            System.out.println("it's not a triangle");
+        }else{
+            System.out.println("this is a valid triangle");
+        }
+    }
 
+    public static void main(String[] args) {
+        TriangleSidesValidator triangleSidesValidator = new TriangleSidesValidator();
+        triangleSidesValidator.validate(7, 8, 29);
     }
 
 }
